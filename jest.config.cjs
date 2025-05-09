7816d2e7f8e09331084a85ac87a347cce2923789
suminhan123 <suminh324@gmail.com>
2025-05-09 09:39:10 +0900

    chore: init jest

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.cjs"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.jest.json",
+      },
+    ],
+    "^.+\\.(js|cjs|jsx)$": "babel-jest",
+  },
+};
---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+// app.test.tsx
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("app test", () => {
+  test("jest title 테스트", () => {
+    render(<App />);
+
+    const titleText = screen.getByTestId("title-text");
+    expect(titleText).not.toBeNull();
+  });
+});
