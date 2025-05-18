438cca24fba4b305b23b2fd169fc52f122f3db3f
suminhan123 <suminh324@gmail.com>
2025-05-18 19:54:32 +0900

    feat: design renderTreeNodePayload props

--- src/Tree/Tree.tsx
+++ src/Tree/Tree.tsx
@@ -1,4 +1,34 @@
-export interface RenderTreeNodePayload {}
+export interface RenderTreeNodePayload {
+  /** Node depth in the tree */
+  depth: number;
+
+  /** `true` if the node is expanded, applicable only for nodes with `children` */
+  expanded: boolean;
+
+  /** `true` if the node has non-empty `children` array */
+  hasChildren: boolean;
+
+  /** `true` if the node is selected */
+  selected: boolean;
+
+  /** Node data from the `data` prop of `Tree` */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  node: any;
+
+  /** Tree controller instance, return value of `useTree` hook */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  tree: any;
+
+  /** Props to spread into the root node element */
+  elementProps: {
+    className: string;
+    style: React.CSSProperties;
+    onClick: (event: React.MouseEvent) => void;
+    "data-selected": boolean | undefined;
+    "data-value": string;
+    "data-hovered": boolean | undefined;
+  };
+}
 
 interface TreeProps<T> {
   /** Data used to render nodes */
@@ -14,6 +44,7 @@ interface TreeProps<T> {
   renderNode?: (payload: RenderTreeNodePayload) => React.ReactNode;
 
   /** Use-tree hook instance that can be used to manipulate component state */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   tree?: any;
 }
 
