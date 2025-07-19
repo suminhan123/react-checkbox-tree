c672d68fa5c5a9e9120f811cc660e463d22f5c2b
suminhan123 <suminh324@gmail.com>
2025-07-19 19:36:31 +0900

    feat: design useTree props

--- src/Tree/useTree.ts
+++ src/Tree/useTree.ts
@@ -1,3 +1,33 @@
-function useTree() {}
+type UseTreeInput = {
+  /** Initial expanded state of all nodes */
+  initialExpandedState?: string[];
+
+  /** Initial selected state of nodes */
+  initialSelectedState?: string[];
+
+  /** Initial checked state of nodes */
+  initialCheckedState?: string[];
+};
+
+type UserTreeReturnType = {
+  checkNode: (node: string) => void;
+  uncheckNode: (node: string) => void;
+
+  unCheckAllNodes: (node: string) => void;
+  checkAllNodes: (node: string) => void;
+
+  toggleExpanded: (node: string) => void;
+
+  isNodeChecked: (node: string) => boolean;
+  isNodeIndeterminate: (node: string) => boolean;
+};
+
+function useTree({
+  initialExpandedState,
+  initialSelectedState,
+  initialCheckedState,
+}: UseTreeInput = {}): UserTreeReturnType {
+  return {};
+}
 
 export { useTree };
